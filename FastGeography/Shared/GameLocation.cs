6afa52df4026ed5c30630db365178d12a888efcd
Vane Spasov <vspasov@tsys.com>
2023-04-29 22:33:30 +0200

    Added url to the location coordinates if it was correct answer.

---
+++ FastGeography/Shared/GameLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace FastGeography.Shared
+{
+    using System;
+
+    public class GameLocation
+    {
+        public LocationType LocationType { get; set; }
+        public string? Answer { get; set; }
+        public int Points { get; set; }
+        public string? Coordinates { get; set; }
+        public Uri? MapsUri => Coordinates == null ? null : new Uri($"https://www.bing.com/maps?cp={Coordinates.Replace(',','~')}&lvl=15");
+        public string? Description { get; set; }
+    }
+}
